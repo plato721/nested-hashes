260f330025d816f31bfcd7917c42bceb6eb602cd
Matt Ewell <matt.ewell@gmail.com>
2022-07-30 13:05:50 -0600

    Minor refactor
    Keep the accumulator in the iteration; don't pass it
    to the function.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Simple Nested Hash Demo
 
-Given a hash whose keys can point to arrays, hashes, strings, or integers, and any of the arrays can contain all of the above as well, write a function that will add up all the numbers contained in the hash.
+Given a hash or array whose values can be arrays, hashes, strings, or integers, and any of the member arrays or hashes can contain all of the above as well, write a function that will add up all the numbers contained in the array/hash.
--- lib/hash_traverse.rb
+++ lib/hash_traverse.rb
@@ -6,20 +6,20 @@ class HashTraverse
   def compute_sum(current_part = @hash)
     if current_part.is_a? Array
       current_part.reduce(0) do |acc, v|
-        process_value(acc, v)
+        acc + process_value(v)
       end
     else
       current_part.reduce(0) do |acc, (_, v)|
-        process_value(acc, v)
+        acc + process_value(v)
       end
     end
   end
 
   private
 
-  def process_value(acc, v)
-    return acc if v.is_a? String
-    return acc + v if v.is_a? Numeric
-    acc + compute_sum(v)
+  def process_value(v)
+    return 0 if v.is_a? String
+    return v if v.is_a? Numeric
+    compute_sum(v)
   end
 end
